fe735c792c18e6bacf24f2e9d0f71dc936a7aa09
jvmvik <jvmvik@gmail.com>
2018-10-11 23:08:10 -0500

    fix ruby implementation  it did not work previously

--- ruby/nokogiri_parser.rb
+++ ruby/nokogiri_parser.rb
@@ -1,14 +1,25 @@
 require 'nokogiri'
 
-def do_time_test(html, n)
+def do_time_test(fn, n, enable_xpath = true)
   start = Time.now
+  links = []
   n.times do
-    tree = Nokogiri::HTML(html)
+    tree = File.open(fn) { |f| Nokogiri::HTML(f) }
     tree.root
+
+    if enable_xpath
+      links = tree.root.xpath('//a')
+    end
   end
   stop = Time.now
 
   puts "#{stop - start} s"
+  puts "#{links.size} links" if enable_xpath
 end
 
-do_time_test File.read(ARGV[0]), ARGV[1].to_i
+enable_xpath = false
+if ARGV.size > 2 and ARGV[2] =~ /true/
+  enable_xpath = true
+end
+
+do_time_test(ARGV[0], ARGV[1].to_i, enable_xpath)
